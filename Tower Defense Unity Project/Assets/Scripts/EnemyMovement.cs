ed64379198c394002bdd432058da4942bfe9f2cd
GOMJA-ROOM <machun3@naver.com>
2019-06-13 10:01:10 +0900

    first commit
    Signed-off-by: GOMJA-ROOM <machun3@naver.com>

---
+++ Tower Defense Unity Project/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Enemy))]
+public class EnemyMovement : MonoBehaviour {
+
+	private Transform target;
+	private int wavepointIndex = 0;
+
+	private Enemy enemy;
+
+	void Start()
+	{
+		enemy = GetComponent<Enemy>();
+
+		target = Waypoints.points[0];
+        this.transform.LookAt(target);
+	}
+
+	void Update()
+	{
+		Vector3 dir = target.position - transform.position;
+		transform.Translate(dir.normalized * enemy.speed * Time.deltaTime, Space.World);
+
+		if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+		{
+			GetNextWaypoint();
+            this.transform.LookAt(target);
+		}
+
+		enemy.speed = enemy.startSpeed;
+	}
+
+	void GetNextWaypoint()
+	{
+		if (wavepointIndex >= Waypoints.points.Length - 1)
+		{
+			EndPath();
+			return;
+		}
+
+		wavepointIndex++;
+		target = Waypoints.points[wavepointIndex];
+	}
+
+	void EndPath()
+	{
+		PlayerStats.Lives--;
+		WaveSpawner.EnemiesAlive--;
+		Destroy(gameObject);
+	}
+
+}
---
+++ Tower Defense Unity Project/Assets/Scripts/sceneChanger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class sceneChanger : MonoBehaviour
+{
+    //public SceneFader sf;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void goToLevel(int lvl) {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Level0" + lvl.ToString());
+        //sf.FadeTo("Level0" + lvl.ToString());
+    }
+}
